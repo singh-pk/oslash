94511a18de73969ea688e95a71634ebb7e9b00a5
singh-pk <pksn1111@gmail.com>
2022-10-29 22:01:31 +0530

    Added Secondary Button in Button.stories.tsx

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -24,6 +24,11 @@ const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
+  children: 'Share'
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
   children: 'Share',
   icon: ShareIcon
 };
