5011d5c5f591b856a118707bf5f785ab4f7e937f
singh-pk <pksn1111@gmail.com>
2022-10-29 21:08:00 +0530

    Added Inter font and configured storybook webpack for sass support

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,5 +10,41 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5'
-  }
+  },
+  webpackFinal: async config => {
+    const scssConfigIndex = config.module.rules.findIndex(c =>
+      '.scss'.match(c.test)
+    );
+
+    if (scssConfigIndex > 0 && config.module.rules[scssConfigIndex]?.oneOf) {
+      config.module.rules[scssConfigIndex].oneOf.push({
+        test: /\.(css|s(a|c)ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../')
+      });
+    } else {
+      config.module.rules.push({
+        test: /\.(css|s(a|c)ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../')
+      });
+    }
+
+    return { ...config, resolve: { ...config.resolve } };
+  },
+  previewHead: head => `
+    ${head}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet">
+
+    <style type="text/css">
+      body {
+        font-family: 'Inter', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 20px;
+      }
+    </style>
+  `
 };
